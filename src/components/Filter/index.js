7e162f0a38a1072a86a3fb6f1a105365a4588bf8
Luis <webdeveloperpr@gmail.com>
2017-11-04 15:10:42 -0400

    Add Filter component

---
+++ src/components/Filter/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Row, Col } from 'react-bootstrap';
+import './styles.css';
+
+const Filter = props => {
+  return (
+    <Row className="filter-wrapper">
+      <Col
+        className="filter-left-section"
+        xs={10}
+        xsPush={1}
+      >
+        <p className="filter-title">{props.title}</p>
+      </Col>
+      <Col
+        className="filter-right-section"
+        xs={2}>
+        <a className="filter-btn">Filter</a>
+      </Col>
+    </Row>
+
+  );
+};
+
+export default Filter;
