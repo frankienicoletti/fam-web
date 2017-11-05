bb123ead4bebf0c023f7e7a6596ade2ae77ab5c4
rjisoo <jisoo.cc@gmail.com>
2017-11-05 03:06:22 -0500

    put src/index.jst back

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-import Calculator from './Components/Tab1';
 
-ReactDOM.render(<Calculator />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
